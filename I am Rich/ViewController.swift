77f9bed14528d6dce5ad623888a1ebe35c684eca
itikhonov <riyuzaky@gmail.com>
2017-10-18 17:38:52 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ivan Tikhonov on 18/10/2017.
+//  Copyright © 2017 Ivan Tikhonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
